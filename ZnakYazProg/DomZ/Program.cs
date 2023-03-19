c4ce4a7d9b091c8624a7d537ea148bf83ee8b53a
wolkstt <wolkstt@gmail.com>
2023-03-19 20:02:15 +0700

    DZ_6

---
+++ ZnakYazProg/DomZ/Program.cs
@@ -0,0 +1,88 @@
+﻿/* 1. Объяви целочисленные переменные A и B
+    int A = 35;
+    int B = 20;
+    double C = Convert.ToDouble(A)/B;
+
+    Console.WriteLine($"Задача_1____Значение A: {A}  Значение B: {B}  A/B  {C}");
+
+// 6. В A и B рандом от 10 до 99
+
+A = new Random().Next(10,99);
+B = new Random().Next(10,99);
+
+Console.WriteLine($"Задача_6____Значение A: {A}  Значение B: {B}");
+
+// 7. Если A > B то в C записываем A / B
+
+if (A > B)
+C = Convert.ToDouble(A)/B;
+if (A < B)
+C = Convert.ToDouble((B)/Convert.ToDouble(A+B));
+if (A == B)
+C = 0;
+
+Console.WriteLine($"Задача_6____{C}");
+*/
+
+/* 2.3 Объявить символьную переменную Зациклить действие пока X не равен символу V
+
+int A;
+int B;
+char x = 'Q';
+double C = 0;
+
+while (x != 'V')
+{
+A = new Random().Next(10,99);
+B = new Random().Next(10,99);
+
+
+if (A > B)
+C = Convert.ToDouble(A)/B;
+if (A < B)
+C = Convert.ToDouble((B)/Convert.ToDouble(A+B));
+if (A == B)
+C = 0;
+
+Console.WriteLine($"Задача_6_________Значение A: {A}  Значение B: {B}  A/B  {C}");
+
+Console.Write("Ввести X: ");
+    x =  Convert.ToChar(Console.ReadLine());
+
+}
+*/
+
+/* 4.
+
+string[] Y = {"3a","2s","5","4g","8"};
+int[] Z = {3,2,5,4,8};
+
+int A;
+int B;
+
+
+
+
+void ShowArray1(string[] array)
+{
+        for(int i = 0; i < array.Length; i++)
+            Console.Write(array[i] + " ");
+
+        Console.WriteLine();
+}
+
+void ShowArray2(int[] array)
+{
+        for(int i = 0; i < array.Length; i++)
+            Console.Write(array[i] + " ");
+
+        Console.WriteLine();
+}
+
+for(int i = 0; i < Y.Length; i++)
+
+    Z[i] = Y[i].Length;
+ 
+ShowArray1(Y);
+ShowArray2(Z);
+*/
---
+++ ZnakYazProg/Sem_6/Program.cs
@@ -0,0 +1,75 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int size = -1; 
+
+while(1 >= size)
+{
+Console.Write("Введите количество элементов массива': ");
+size =  Convert.ToInt32(Console.ReadLine());
+}
+
+// Console.Write("Введите количество элементов массива': ");
+//     int size =  Convert.ToInt32(Console.ReadLine());
+    
+    int N = 0;
+    int[] array = new int[size]; //= CreateRandomArray(size,0,99);
+
+// ShowArray(array);
+
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write("Введите элемент массива': ");
+        array[i] =  Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0)
+        N = N + 1;
+}
+    void ShowArray(int[] array)
+{
+        for(int i = 0; i < array.Length; i++)
+            Console.Write(array[i] + " ");
+
+        Console.WriteLine();
+}
+ShowArray(array);
+Console.Write("Количество элементов массива > больше 0 : " +N);
+
+//
+//
+*/
+
+ /* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+        if (k1 == k2)
+        {
+            Console.WriteLine("Прямые параллельны, пересечения нет");
+        }
+        else
+        {
+            double X; 
+                X = (b2 - b1) / (k1 - k2);
+            double Y1; 
+                Y1 = k1 * X + b1;
+            double Y2;
+                Y2 = k2 * X + b2;
+            Console.WriteLine($"Прямые пересекаются, Точка пересечения :  {X} {Y1}");
+        }
+   
+   // Console.WriteLine($"Прямые пересекаются, Точка пересечения :  {X} {Y1}");
+
+// Console.WriteLine($"Точка пересечения :  {X} {Y1}");
+
+*/
